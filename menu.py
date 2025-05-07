acc58af2ed9612ee0e85c144294ea0df5125b569
thiagozaneti <dotinha0203@gmail.com>
2025-05-06 22:11:30 -0300

    initial commit in menu branch

---
+++ menu.py
@@ -0,0 +1,26 @@
+##Criação de arquivo de centralizacao de todos os menus
+
+def initial_menu():
+    print("""
+        Seja bem vindo a calculadora de determinante de matrizes
+        --------------------------------------------------
+        Qual determinante você deseja calcular?
+        [1]Determinante de matriz 1x1
+        [2]Determinante de matriz 2x2
+        [3]Determinante de matriz 3x3
+        [0]Sair 
+    """)
+def determinante_1x1_menu():
+    print("""
+        Digite um valor para a matriz 1x1: 
+    """)
+def determinante_2x2_menu():
+    print("""
+        Digite quatro valores para a matriz 2x2
+""")
+
+def determinante_3x3_menu():
+    print("""
+        Digite nove valores para a matriz 3x3
+""")
+    
