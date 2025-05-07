3ab2c1396c7b2a28f7e5ed5cdbdec3d6ba5d41d7
thiagozaneti <dotinha0203@gmail.com>
2025-05-06 22:19:01 -0300

    adição de while com case para usuário digitar sua seleção

---
+++ main.py
@@ -0,0 +1,22 @@
+from menu import initial_menu, determinante_1x1_menu, determinante_2x2_menu, determinante_3x3_menu
+
+def logic():
+    while True:
+        initial_menu()
+        initial_user_select = int(input("Digite sua seleção"))
+        match initial_user_select:
+            case 0:
+                break
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case _:
+                break
+
+            
+            
+
+
