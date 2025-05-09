d0ba90b4d1b2f6937ae623033385decf018a5c92
thiagozaneti <dotinha0203@gmail.com>
2025-05-09 14:27:10 -0300

    adição de conta de diagonais no menu

--- menu.py
+++ menu.py
@@ -1,3 +1,10 @@
+def initial_menu():
+    print("\n=== CALCULADORA DE DETERMINANTES ===")
+    print("1. Determinante de matriz 1x1")
+    print("2. Determinante de matriz 2x2")
+    print("3. Determinante de matriz 3x3")
+    print("0. Sair")
+
 def determinante_1x1_menu():
     valor = float(input("Digite o único valor da matriz 1x1: "))
     print(f"Determinante: {valor}")
@@ -22,3 +29,17 @@ def determinante_3x3_menu():
     a31 = float(input("a31: "))
     a32 = float(input("a32: "))
     a33 = float(input("a33: "))
+
+    diagonais_principais = (
+        a11 * a22 * a33 +
+        a12 * a23 * a31 +
+        a13 * a21 * a32
+    )
+    diagonais_secundarias = (
+        a13 * a22 * a31 +
+        a11 * a23 * a32 +
+        a12 * a21 * a33
+    )
+
+    determinante = diagonais_principais - diagonais_secundarias
+    print(f"Determinante: {determinante}")
