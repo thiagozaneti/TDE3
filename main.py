13bda25131467b43d519845117fbd83751ffdc09
thiagozaneti <dotinha0203@gmail.com>
2025-05-09 14:29:51 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from menu import initial_menu, determinante_1x1_menu, determinante_2x2_menu, determinante_3x3_menu
-
 def logic():
     while True:
         initial_menu()
-        initial_user_select = int(input("Digite sua seleção"))
+        initial_user_select = int(input("Digite sua escolha: "))
         match initial_user_select:
             case 0:
+                print("Encerrando o programa.")
                 break
             case 1:
-                pass
+                determinante_1x1_menu()
             case 2:
-                pass
+                determinante_2x2_menu()
             case 3:
-                pass
+                determinante_3x3_menu()
             case _:
-                break
+                print("Opção inválida. Tente novamente.")
 
             
-            
+if __name__ == "__main__":
+    logic()
 
 
